4ecb7d3ed1b04e1fc8cba4046e8f1e33a7102825
IhsanuRahman <ihsanurahman8055@gmail.com>
2024-09-08 14:51:43 +0530

    sort label

--- house-of-tea/src/components/Menu/Menu.js
+++ house-of-tea/src/components/Menu/Menu.js
@@ -1114,7 +1114,7 @@ const Menu = () => {
       <div className={`content ${!sidebarVisible ? "expanded" : ""}`}>
         <h2 className="category-title">{loading && !sidebarVisible ? 'loading..' : selectedCategory}</h2>
         <div className="sort-section">
-          <p>select sort:</p>
+          <p>Select price variant:</p>
           <select className="sort-selection select" name="" id="" onChange={onSortChange} value={sortValue}>
             <option value="default" >select option</option>
             <option value="price-low">low price</option>
